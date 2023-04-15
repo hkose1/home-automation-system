3412103d2cbbbfbba907a52150157ce85d029d5a
Haydar <haydarkose86@gmail.com>
2023-04-15 23:51:57 +0300

    modified signup req

--- controller/controller.php
+++ controller/controller.php
@@ -19,6 +19,20 @@ if (get('req') == 'signup') {
         $_SESSION['required_field_error'] = "* Fields are required.";
         header("Location:../signup/signup.php");
         exit();
+    }else if (is_username_used($username)) {
+        $_SESSION['username_taken_error'] = "This username is already taken.";
+        header("Location:../signup/signup.php");
+        exit();
+    }else {
+        $is_added = add_user($username, $password, $email, $phone ?? null);
+        if ($is_added) {
+            $_SESSION['succeessfully_signedup'] = "You have successfully signed up";
+            header("Location:../signup/signup.php");
+        }else {
+            $_SESSION['error'] = "Something went wrong.";
+            header("Location:../signup/signup.php");
+            exit();
+        }
     }
 }
 
