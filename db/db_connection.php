98590923dc109d9d19f2cfb47b207de05a9d16e9
Haydar <haydarkose86@gmail.com>
2023-04-16 00:25:58 +0300

    added get_user func. to check whether there is a user or not with username and password

--- db/db_connection.php
+++ db/db_connection.php
@@ -16,10 +16,21 @@ function add_user($username, $pass, $email, $phone) {
         $q = $db->prepare("INSERT INTO user SET username = ?, password = ?, email = ?, phone_no = ?");
         $is_added = $q->execute([$username, md5($pass), $email, $phone]);
         if ($is_added) {
-            return true;
+            return $q->rowCount() > 0;
         }else {
-            return false;
+            throw new Exception("An error has occured while signing.");
         }
+    }
+}
+
+function get_user($username, $pass) {
+    global $db;
+    $q = $db->prepare('SELECT * FROM user WHERE username = ? AND password = ?');
+    $user = $q->execute([$username, md5($pass)]);
+    if ($user) {
+        return $q->rowCount() > 0;
+    }else {
+        throw new Exception("An error has occured while getting user");
     }
 }
 
@@ -28,11 +39,7 @@ function is_username_used($username) {
     $users = $db->prepare('SELECT * FROM user WHERE username = ?');
     $res = $users->execute([$username]);
     if ($res) {
-        if ($users->rowCount()) {
-            return true;
-        }else {
-            return false;
-        }
+        return $users->rowCount() > 0;
     }else {
         throw new Exception("An error has occured while checking usernames in db");
     }
