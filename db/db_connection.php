6642e75f4ca7fa4bbb5b61eff19c4dfe6fe70b3c
Haydar <haydarkose86@gmail.com>
2023-04-15 23:02:18 +0300

    added add_user and is_username_used functions in db_connection

--- db/db_connection.php
+++ db/db_connection.php
@@ -8,4 +8,34 @@ try {
     echo $e->getMessage();
 }
 
+function add_user($username, $pass, $email, $phone) {
+    if (is_username_used($username)) {
+        return false;
+    } else {
+        global $db;
+        $q = $db->prepare("INSERT INTO user SET username = ?, password = ?, email = ?, phone_no = ?");
+        $is_added = $q->execute([$username, md5($pass), $email, $phone]);
+        if ($is_added) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
+
+function is_username_used($username) {
+    global $db;
+    $users = $db->prepare('SELECT * FROM user WHERE username = ?');
+    $res = $users->execute([$username]);
+    if ($res) {
+        if ($users->rowCount()) {
+            return true;
+        }else {
+            return false;
+        }
+    }else {
+        throw new Exception("An error has occured while checking usernames in db");
+    }
+}
+
 ?>
