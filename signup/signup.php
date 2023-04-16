a7cd471fe69440e6be707452fc36e46e370e5909
Haydar <haydarkose86@gmail.com>
2023-04-16 16:41:01 +0300

    added some styling on sign up page using bootstrap

--- signup/signup.php
+++ signup/signup.php
@@ -10,45 +10,72 @@ include '../utils/utils.php';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="signup.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <title>Sign Up</title>
 </head>
 <body>
-    <main>
-        <div class="wrapper">
+    <main class="container d-flex align-items-center justify-content-center" style="margin-top: 7rem;">
+        <div class="card" style="width: 30rem;">
+            <div class="card-header">
+                Sign up
+            </div>
 
-        <?php if(session('error')): ?>
-            <div class="error"><?= session('error') ?></div>
+            <div class="card-body">
 
-        <?php elseif(session('required_field_error')): ?>
-            <div class="error"><?= session('required_field_error') ?></div>
+            <?php if(session('error')): ?>
+                <div class="alert alert-danger"><?= session('error') ?></div>
 
-        <?php elseif(session('username_taken_error')): ?>
-            <div class="error"><?= session('username_taken_error') ?></div>
+            <?php elseif(session('required_field_error')): ?>
+                <div class="alert alert-danger"><?= session('required_field_error') ?></div>
 
-        <?php elseif(session('succeessfully_signedup')): ?>
-            <div class="success"><?= session('succeessfully_signedup') ?></div>
-        <?php endif ?>
+            <?php elseif(session('username_taken_error')): ?>
+                <div class="alert alert-danger"><?= session('username_taken_error') ?></div>
+
+            <?php elseif(session('succeessfully_signedup')): ?>
+                <div class="alert alert-success"><?= session('succeessfully_signedup') ?></div>
+            <?php endif ?>
         
         
-        <form action="../controller/controller.php?req=signup" method="post">
-            <label for="username">Username</label><span> *</span><br>
-            <input type="text" name="username" id="username" value="<?= session('username') ?>" placeholder="username"><br><br>
+            <form action="../controller/controller.php?req=signup" method="post">
+                
+                <label for="username" class="col-sm-3 col-form-label">Username<span style="color:red"> *</span></label>
+                <div class="col-sm-9">
+                    <input type="text" name="username" id="username" 
+                        class="form-control"
+                        value="<?= session('username') ?>" placeholder="username">
+                </div>
 
-            <label for="password">Password</label><span> *</span><br>
-            <input type="password" name="password" id="password" placeholder="********"><br><br>
+                <label for="password" class="col-sm-3 col-form-label">Password<span style="color:red"> *</span></label>
+                <div class="col-sm-9">
+                    <input type="password" name="password" id="password"
+                        class="form-control"
+                        placeholder="********">
+                    </div>
 
-            <label for="email">Email</label><span> *</span><br>
-            <input type="email" name="email" id="email" value="<?= session('email') ?>" placeholder="example@gmail.com"><br><br>
 
-            <label for="phone">Phone Number</label><br>
-            <input type="tel" name="phone" id="phone" 
-                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
-                placeholder="xxx-xxx-xxxx"
-                value="<?= session('phone') ?>"><br><br>
+                <label for="email" class="col-sm-3 col-form-label">Email<span style="color:red"> *</span></label>
+                <div class="col-sm-9">
+                    <input type="email" name="email" id="email" value="<?= session('email') ?>" 
+                        class="form-control"
+                        placeholder="example@gmail.com">
+                </div>
 
-            <button type="submit">Sign up</button><a href="../login/login.php">Log in</a><br><br>
-        </form>
+
+                <label for="phone" class="col-sm-3 col-form-label">Phone Number</label>
+                <div class="col-sm-9">
+                    <input type="tel" name="phone" id="phone" 
+                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                        class="form-control"
+                        placeholder="xxx-xxx-xxxx"
+                        value="<?= session('phone') ?>">
+                </div>
+                <div style="display: flex; justify-content:flex-start; align-items: center">
+                <button type="submit" class="btn btn-success mb-4" style="margin: 23px 5px">Sign up</button>
+                <a href="../login/login.php" class="btn btn-primary">Log in</a><br><br>
+                </div>
+            </form>
+            </div>
         </div>
     </main>
 </body>
