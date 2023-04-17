b7cdaac730943435ea5895aa3de3eb2530e46b1f
Haydar <haydarkose86@gmail.com>
2023-04-18 00:26:41 +0300

    added some style

--- login/login.php
+++ login/login.php
@@ -13,11 +13,19 @@ include '../utils/utils.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <title>Log in</title>
+    <style>
+        body {
+            background: #527a7a;
+        }
+        .card-bg {
+            background: #c2f0f0;
+        }
+    </style>
 </head>
 <body>
     <main class="container d-flex align-items-center justify-content-center" style="margin-top: 7rem;">
         
-        <div class="card" style="width: 30rem;">
+        <div class="card card-bg" style="width: 30rem;">
             <div class="card-header">
                 <strong>Log in</strong>
             </div>
