d0b4cc2d5bea0199485a2b5000de4dc1b4f3b42c
Haydar <haydarkose86@gmail.com>
2023-04-17 15:41:17 +0300

    updated

--- index.php
+++ index.php
@@ -47,6 +47,9 @@ if (!isset($_SESSION['login']) || $_SESSION['login'] == false) {
         .table th td {
             margin-right: 4px;
         }
+        .device-icon {
+            width: 10rem;
+        }
         
     </style>
 </head>
@@ -73,13 +76,13 @@ if (!isset($_SESSION['login']) || $_SESSION['login'] == false) {
                                     <td> 08%</td>
                                 </tr>
                             </table>
-                            <a href="#" class="btn btn-info">Details</a>    
+                            <a href="index.php?room=1" class="btn btn-info">Details</a>    
                         </div>
                     </div>
                 </li>
                 <li id="bedroom">
                     <div class="card my-card">
-                        <img src="./image/bedroom.avif" class="card-image-top" alt="living-room"/>
+                        <img src="./image/bedroom.avif" class="card-image-top" alt="bedroom"/>
                         <div class="card-body">
                             <h5 class="card-title">Bedroom</h5>
                             <table class="table">
@@ -92,13 +95,13 @@ if (!isset($_SESSION['login']) || $_SESSION['login'] == false) {
                                     <td> 08%</td>
                                 </tr>
                             </table>
-                            <a href="#" class="btn btn-info">Details</a>
+                            <a href="index.php?room=2" class="btn btn-info">Details</a>
                         </div>
                     </div>
                 </li>
                 <li id="bathroom">
                     <div class="card my-card">
-                        <img src="./image/bathroom.avif" class="card-image-top" alt="living-room"/>
+                        <img src="./image/bathroom.avif" class="card-image-top" alt="bathroom"/>
                         <div class="card-body">
                             <h5 class="card-title">Bathroom</h5>
                             <table class="table">
@@ -111,13 +114,13 @@ if (!isset($_SESSION['login']) || $_SESSION['login'] == false) {
                                     <td> 08%</td>
                                 </tr>
                             </table>
-                            <a href="#" class="btn btn-info">Details</a>
+                            <a href="index.php?room=3" class="btn btn-info">Details</a>
                         </div>
                     </div>
                 </li>
                 <li id="kitchen">
                     <div class="card my-card">
-                        <img src="./image/kitchen.avif" class="card-image-top" alt="living-room"/>
+                        <img src="./image/kitchen.avif" class="card-image-top" alt="kitchen"/>
                         <div class="card-body">
                             <h5 class="card-title">Kitchen</h5>
                             <table class="table">
@@ -130,7 +133,7 @@ if (!isset($_SESSION['login']) || $_SESSION['login'] == false) {
                                     <td> 08%</td>
                                 </tr>
                             </table>
-                            <a href="#" class="btn btn-info">Details</a>
+                            <a href="index.php?room=4" class="btn btn-info">Details</a>
                         </div>
                     </div>
                 </li>
@@ -148,11 +151,88 @@ if (!isset($_SESSION['login']) || $_SESSION['login'] == false) {
                     </li>
                 </ul>
             </div>
-            <!-- adfs -->
+            <div class="d-flex justify-content-around flex-wrap">
+                <div class="card" style="width: 23rem;">
+                    <div class="card-header">
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="ac">
+                            <label class="form-check-label" for="ac">Air Conditioner</label>
+                        </div>
+                    </div>
+                    <div class="card-body d-flex align-items-center justify-content-center flex-column">
+                        <img src="./image/devices/air-conditioner.avif" class="card-image-top device-icon"alt="air-conditioner"/>
+                        <div>
+                            <input type="range" name="ac-level" id="ac-level">
+                            <span>Value: <output id="ac-value"></output></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card" style="width: 23rem;">
+                    <div class="card-header">
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" role="switch" id="tv">
+                        <label class="form-check-label" for="tv">Television</label>
+                    </div>
+                    </div>
+                    <div class="card-body d-flex align-items-center justify-content-center flex-column">
+                        <img src="./image/devices/television2.avif" class="card-image-top device-icon" alt="television"/>
+                    </div>
+                </div>
+                <div class="card" style="width: 23rem;">
+                    <div class="card-header">
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" role="switch" id="audio-system">
+                        <label class="form-check-label" for="audio-system">Audio System</label>
+                    </div>
+                    </div>
+                    <div class="card-body">
+                        
+                    </div>
+                </div>
+                <div class="card" style="width: 23rem;">
+                    <div class="card-header">
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" role="switch" id="window">
+                        <label class="form-check-label" for="window">Windows</label>
+                    </div>
+                    </div>
+                    <div class="card-body">
+                        
+                    </div>
+                </div>
+                <div class="card" style="width: 23rem;">
+                    <div class="card-header">
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" role="switch" id="lamp">
+                        <label class="form-check-label" for="lamp">Lamp</label>
+                    </div>
+                    </div>
+                    <div class="card-body">
+                        
+                    </div>
+                </div>
+                <div class="card" style="width: 23rem;">
+                    <div class="card-header">
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" role="switch" id="curtain">
+                        <label class="form-check-label" for="curtain">Curtain</label>
+                    </div>
+                    </div>
+                    <div class="card-body">
+                        
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
     <script>
-
+        const value = document.querySelector("#ac-value")
+        const input = document.querySelector("#ac-level")
+        value.textContent = input.value
+        input.addEventListener("input", (event) => {
+        value.textContent = event.target.value
+        })
     </script>
 </body>
 </html>
