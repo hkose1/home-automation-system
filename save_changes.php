52151a138719e4c955431a21a1e5d6b3eaaabde6
Haydar <haydarkose86@gmail.com>
2023-04-19 05:18:43 +0300

    added a php file called save_changes to save user data to the db. Getting ajax post values

---
+++ save_changes.php
@@ -0,0 +1,48 @@
+<?php
+include './utils/utils.php';
+include './db/db_devices.php';
+
+$content = trim(file_get_contents("php://input"));
+$data = json_decode($content, true);
+
+
+$room_id = $data['room_id'];
+$state = $data['state'];
+$rangeValue = $data['range_value'];
+$which_device = $data['device'];
+
+
+$final_state = 0;
+if (isset($state) && !empty($state) && $state != false) {
+    $final_state = 1;
+}
+
+switch ($which_device) {
+    case 'ac':
+        set_ac_device_state($room_id, $final_state);
+        break;
+    case 'tv':
+        set_tv_device_state($room_id, $final_state);
+        break;
+    case 'audio':
+        set_audio_device_state($room_id, $final_state);
+        break;
+    case 'window':
+        set_window_device_state($room_id, $final_state);
+        break;
+    case 'lamp':
+        set_lamp_device_state($room_id, $final_state);
+        break;
+    case 'curtain':
+        set_curtain_device_state($room_id, $final_state);
+        break;
+}
+
+// if ($rangeValue) {
+//     // $final_rangeValue = 0;
+//     // if (isset($rangeValue) && !empty($rangeValue) && $rangeValue != false) {
+//     //     $final_rangeValue = 1;
+//     // }
+//     // set_ac_device_state($room_id, $final_state);
+//     set_ac_device_value($room_id, $rangeValue);
+// }
