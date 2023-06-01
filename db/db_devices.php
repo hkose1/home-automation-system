9430559263c0fc307c76f317f002d273a39e4442
Haydar <haydarkose86@gmail.com>
2023-06-01 18:07:57 +0300

    setting ac device mood on mysql database

--- db/db_devices.php
+++ db/db_devices.php
@@ -48,6 +48,11 @@ function set_ac_device_value($id, $value) {
     $q = $db->prepare("UPDATE device_ac SET value = ? WHERE room_id = ?");
     $q->execute([$value, $id]);
 }
+function set_ac_device_mood($id, $mood_value) {
+    global $db;
+    $q = $db->prepare("UPDATE device_ac SET mood = ? WHERE room_id = ?");
+    $q->execute([$mood_value, $id]);
+}
 
 // television
 function get_tv_device($room_id) {
